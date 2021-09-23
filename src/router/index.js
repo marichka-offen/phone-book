67428cbd5702adb52ddd51ab8951868946d3358f
Marichka Offen <marichka.offen@gmail.com>
2021-09-23 11:29:39 -0400

    Created Views and routes for them

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,42 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
 
+import AllContacts from "@/views/AllContacts.vue"
+import Family from "@/views/Family.vue"
+import Friends from "@/views/Friends.vue"
+import Work from "@/views/Work.vue"
+import Other from "@/views/Other.vue"
+
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: "/",
+    name: "AllContacts",
+    component: AllContacts
+  },
+  {
+    path: "/family",
+    name: "Family",
+    component: Family
+  },
+  {
+    path: "/friends",
+    name: "Friends",
+    component: Friends
+  },
+  {
+    path: "/work",
+    name: "Work",
+    component: Work
+  },
+  {
+    path: "/other",
+    name: "Other",
+    component: Other
+  }
+  //   { path: "*", component: PageNotFound }
+]
 
 const router = new VueRouter({
   mode: "history",
