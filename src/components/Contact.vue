adfaadd1b839c7d510ead9cafbaf46c80df39b61
Marichka Offen <marichka.offen@gmail.com>
2021-09-23 17:04:52 -0400

    Created view for contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>{{ contact.name }} {{ contact.category }}</div>
+  <div id="contact">
+    <div class="d-flex">
+      <img class="avatar" :src="contact.avatar" />
+      <div>
+        <p class="name">{{ contact.name }}</p>
+        <p class="address">
+          {{ contact.address.street }}, {{ contact.address.city }}
+        </p>
+      </div>
+    </div>
+    <div class="contacts">
+      <p>{{ contact.email }}</p>
+      <p>{{ contact.phone }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,4 +27,43 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+// variables
+$color-grey-light-2: #f4f2f2;
+$color-grey-dark: #999;
+
+#contact {
+  padding: 10px;
+  margin: 0 10px;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid $color-grey-light-2;
+
+  & .avatar {
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+    margin-right: 15px;
+  }
+
+  & .name {
+    color: #004d7a;
+    font-size: 15px;
+    margin-top: 10px;
+    text-transform: uppercase;
+    font-weight: 600;
+  }
+
+  & .address {
+    font-size: 12px;
+    color: $color-grey-dark;
+  }
+
+  & .contacts {
+    text-align: right;
+    font-size: 12px;
+    margin-top: 10px;
+    color: $color-grey-dark;
+  }
+}
+</style>
