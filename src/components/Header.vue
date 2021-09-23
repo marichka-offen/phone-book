3b924d96edc40b0dbdce0302abf7ca4f1ef3e38a
Marichka Offen <marichka.offen@gmail.com>
2021-09-23 13:21:44 -0400

    Create header component with searchbar

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="header">
+    <slot name="header"> </slot>
+    <search-bar />
+  </div>
+</template>
+
+<script>
+import SearchBar from "@/components/SearchBar.vue"
+
+export default {
+  components: { SearchBar }
+}
+</script>
+
+<style lang="scss" scoped>
+// variables
+$color-grey-light: #faf9f9;
+$primary-color: #00bf72;
+$primary-dark: #008793;
+
+#header {
+  margin: 20px;
+  color: $primary-color;
+  text-transform: uppercase;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & > * {
+    margin: 0;
+    font-size: 24px;
+    font-weight: 300;
+    letter-spacing: 1px;
+  }
+}
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <form action="#" class="search">
+    <input type="text" placeholder="Search contacts.." />
+
+    <i class="fas fa-search"></i>
+  </form>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+// variables
+$color-grey-light-2: #f4f2f2;
+$color-grey-light-3: #f0eeee;
+$color-grey-light-4: #ccc;
+
+.search {
+  flex: 0 0 40%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & input {
+    font-size: 15px;
+    color: inherit;
+    background-color: $color-grey-light-2;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 100px;
+    width: 90%;
+    transition: all 0.2s;
+    margin-right: -30px;
+
+    &:focus {
+      outline: none;
+      width: 100%;
+      background-color: $color-grey-light-3;
+    }
+
+    &::-webkit-input-placeholder {
+      font-weight: 100;
+
+      color: $color-grey-light-4;
+      margin-bottom: 20px;
+    }
+  }
+
+  & input:focus {
+    background-color: $color-grey-light-3;
+  }
+
+  & i {
+    font-size: 20px;
+  }
+}
+</style>
