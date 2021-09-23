0f73a06a0b5ac09ef6402cf7af90ec6666c1d901
Marichka Offen <marichka.offen@gmail.com>
2021-09-23 15:26:34 -0400

    created contact component to style it separately

---
+++ src/components/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ contact.name }} {{ contact.category }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    contact: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
