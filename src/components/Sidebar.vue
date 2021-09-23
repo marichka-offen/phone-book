df5641f9dd9564a1885c5354ed271dc211c45299
Marichka Offen <marichka.offen@gmail.com>
2021-09-23 11:31:13 -0400

    Created sidebar and connected it to router

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,124 @@
+<template>
+  <div id="left-drawer">
+    <div class="d-flex justify-content-center">
+      <button class="add-user-btn">
+        <i class="fas fa-user-plus"></i>
+      </button>
+    </div>
+    <hr class="divider" />
+    <div class="nav-links">
+      <router-link to="/"
+        ><i class="fas fa-globe-europe"></i
+        ><span>All Contacts</span></router-link
+      >
+      <router-link to="/family"
+        ><i class="fas fa-home"></i><span>Family</span></router-link
+      >
+      <router-link to="/friends">
+        <i class="fas fa-users"></i><span>Friends</span></router-link
+      >
+      <router-link to="/work">
+        <i class="fas fa-briefcase"></i><span>Work</span></router-link
+      >
+      <router-link to="/other">
+        <i class="fas fa-address-book"></i><span>Other</span></router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+// variables
+$color-grey-light: #faf9f9;
+$primary-color: #00bf72;
+$primary-dark: #008793;
+
+#left-drawer {
+  width: 220px;
+  height: 100%;
+  background-color: #051937;
+
+  & .add-user-btn {
+    color: $color-grey-light;
+    border: 2px solid $color-grey-light;
+    font-size: 20px;
+    width: 45px;
+    height: 45px;
+    border-radius: 50%;
+    margin: 15px auto;
+    transition: all 0.25s;
+
+    &:hover {
+      color: $primary-color;
+      border-color: $primary-color;
+    }
+
+    & :active::before {
+      color: $primary-dark;
+    }
+  }
+
+  & .divider {
+    height: 1px;
+    color: $color-grey-light;
+    margin: 0 10px;
+  }
+
+  & .nav-links {
+    & a {
+      position: relative;
+      display: flex;
+      align-items: center;
+      margin: 10px 0;
+      padding: 10px 10px;
+    }
+
+    & i {
+      color: $color-grey-light;
+      font-size: 18px;
+      opacity: 0.8;
+      width: 30px;
+      text-align: center;
+    }
+
+    & a::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 3px;
+      background-color: $primary-color;
+      transform: scaleY(0);
+      transition: transform 0.2s, width 0.4s cubic-bezier(1, 0, 0, 1) 0.2s,
+        background-color 0.1s;
+    }
+
+    & a:hover::before,
+    & a.router-link-exact-active::before {
+      transform: scaleY(1);
+      width: 100%;
+    }
+
+    & a:active::before {
+      background-color: $primary-dark;
+    }
+
+    & span {
+      font-size: 14px;
+      text-transform: uppercase;
+      letter-spacing: 1px;
+      font-weight: 200;
+      margin-left: 10px;
+      display: inline-block;
+      color: $color-grey-light;
+      position: relative;
+      z-index: 10;
+    }
+  }
+}
+</style>
