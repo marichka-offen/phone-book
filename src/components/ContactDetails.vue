41f1f5044ddfe5d2d0dafc7d2ed8a07f5d1ad63e
Marichka Offen <marichka.offen@gmail.com>
2021-09-23 11:31:38 -0400

    Component for content details

---
+++ src/components/ContactDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="right-drawer"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+// variables
+$color-grey-light: #faf9f9;
+
+#right-drawer {
+  width: 250px;
+  height: 100%;
+  background-color: $color-grey-light;
+}
+</style>
